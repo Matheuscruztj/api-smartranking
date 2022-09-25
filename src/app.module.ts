c6f3a0c8dd4baecfdde2b7695e6d3345ac367ba5
Matheus Santos <matheus.cbmdk@gmail.com>
2022-09-25 18:00:23 -0300

    refactor: add to config

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { DesafiosModule } from './desafios/desafios.module';
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@cluster0.vbczdl9.mongodb.net/smartranking?retryWrites=true&w=majority`,
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
         useUnifiedtopology: true,
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
-import * as momentTimezone from 'moment-timezone'
+import * as momentTimezone from 'moment-timezone';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
